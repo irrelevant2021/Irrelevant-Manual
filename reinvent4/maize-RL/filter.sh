52c0dd175a56be04fe14eab49f7bbf7251e01037
irrelevant2021 <92629636+irrelevant2021@users.noreply.github.com>
2024-12-18 09:43:55 +0800

    Update filter.sh

--- reinvent4/maize-RL/filter.sh
+++ reinvent4/maize-RL/filter.sh
@@ -7,7 +7,7 @@ do
 	awk '/\$\$\$\$/ {n++; next} {print > "filter/" substr(FILENAME, 1, length(FILENAME)-3) n ".sdf"}' "$i"
 done
 
-for i in $(cat filter.csv) ;do grep -F $i filter/*sdf; done
+for i in $(cat filter.csv) ;do grep -x -F $i filter/*sdf; done
 
 #filter_sdf.txt is from Sublime after grep
 #for i in $(cat filter_sdf.txt) ;do cat $i >> filter/result.sdf; echo '$$$$' >> filter/result.sdf; done
