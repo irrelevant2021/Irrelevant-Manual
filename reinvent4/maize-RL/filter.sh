dbf0ca2c3ff2993096558ec12e0bf9b6ff15c8af
irrelevant2021 <92629636+irrelevant2021@users.noreply.github.com>
2024-12-18 09:46:55 +0800

    Update filter.sh

--- reinvent4/maize-RL/filter.sh
+++ reinvent4/maize-RL/filter.sh
@@ -7,7 +7,8 @@ do
 	awk '/\$\$\$\$/ {n++; next} {print > "filter/" substr(FILENAME, 1, length(FILENAME)-3) n ".sdf"}' "$i"
 done
 
-for i in $(cat filter.csv) ;do grep -x -F $i filter/*sdf; done
+#filter_smiles.csv is from df['SMILES'] of filter.csv 
+#for i in $(cat filter_smiles.csv) ;do grep -x -F $i filter/*sdf; done
 
 #filter_sdf.txt is from Sublime after grep
 #for i in $(cat filter_sdf.txt) ;do cat $i >> filter/result.sdf; echo '$$$$' >> filter/result.sdf; done
