a1b0ab2347964ecd9af8bd498440c97f340eb5a9
irrelevant2021 <92629636+irrelevant2021@users.noreply.github.com>
2025-04-24 17:07:16 +0800

    run all pairs in map at once

---
+++ openfe/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for file in transformations/*.json; do
+  relpath=${file:16}  # strip off "network_setup/transformations/"
+#  echo $relpath
+  dirpath=${relpath%.*}  # strip off final ".json"
+#  echo $dirpath
+  openfe quickrun $file -o results/$relpath -d results/$dirpath
+done
