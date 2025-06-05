d31e7b142695bcd933ce922b914d2dbd31523e1a
irrelevant2021 <92629636+irrelevant2021@users.noreply.github.com>
2025-06-05 16:28:30 +0800

    Delete TIES directory
    replaced by openfe

--- TIES/mod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sed -i  's/sampling_per_window = 4/sampling_per_window = 0.4/'  ./ties/ties*/*/TIES.cfg
-sed -i  's/equili_per_window = 2/equili_per_window = 0.1/'  ./ties/ties*/*/TIES.cfg
-sed -i  's/total_reps = 5/total_reps = 12/'  ./ties/ties*/*/TIES.cfg
-sed -i  's/split_run = 1/split_run = 0/'  ./ties/ties*/*/TIES.cfg
--- TIES/print.py
+++
@@ -1,9 +0,0 @@
-import pprint
-import ast
-
-with open('result.dat', 'r') as file:
-    data = file.read()
-
-data_dict = ast.literal_eval(data)
-
-pprint.pprint(data_dict)
